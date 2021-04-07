bc72ae16f103eb50e605098cd0cbd1443fb8c00a
Jaime Garcia Garcia <jgarcia16432@gmail.com>
2021-04-06 22:18:56 -0700

    Add an instructor table for instructor view

--- src/views/Instructor.vue
+++ src/views/Instructor.vue
@@ -1,18 +1,63 @@
 <template>
   <div>
     <h1>Instructor View</h1>
+    <table>
+      <thead>
+        <tr>
+          <td>Course ID</td>
+          <td>Course Name</td>
+          <td>Course Type</td>
+          <td>Location</td>
+          <td>Unit</td>
+          <td>Seats Left</td>
+          <td>Generate Permission Number</td>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, i) in instructorClass" :key="i">
+          <td>
+            {{ item.class_number }}
+          </td>
+          <td>
+            {{ item.subject }} {{ item.catalog_number }} - {{ item.title }}
+          </td>
+          <td>
+            {{ item.class_type }}
+          </td>
+          <td>{{ item.meetings[0].location }}</td>
+          <td>{{ item.units }}</td>
+          <td>{{ item.enrollment_cap - item.enrollment_count }}</td>
+          <td><button>Generate</button></td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
 <script>
-/*ADD THE FOLLOWING ROUTE IN router.js
-{
-  path: '/instructor',
-  name: 'instructor',
-  component: () => import('./views/Instructor.vue')
-},
-*/
+import Vue from "vue";
+import axios from "axios";
+import VueAxios from "vue-axios";
+Vue.use(VueAxios, axios);
+
 export default {
   name: "Instructor",
+  data: function () {
+    return {
+      firstName: "Vincent",
+      lastName: "Doe",
+      instructorClass: undefined,
+    };
+  },
+  mounted: function () {
+    Vue.axios
+      .get(
+        "https://api.metalab.csun.edu/curriculum/api/2.0/classes?instructor=vahe.karamian@csun.edu"
+      )
+      .then((resp) => {
+        this.instructorClass = resp.data.classes;
+        console.log(resp);
+      });
+  },
 };
 </script>
